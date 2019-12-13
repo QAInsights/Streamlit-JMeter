eab0a7e9306022b1fcc1c42b0abf95b37d82a8b5
NaveenKumar <catch.nkn@gmail.com>
2019-12-12 22:31:49 -0500

    Draft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@
 
 
 # -- Project information -----------------------------------------------------
-import sphinx_rtd_theme
 project = 'Streamlit-JMeter'
 copyright = '2019, NaveenKumar Namachivayam'
 author = 'NaveenKumar Namachivayam'
@@ -46,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
