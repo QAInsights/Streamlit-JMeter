ecc5c78c2459f35a488a2e081bc09bf7df96ade5
NaveenKumar <catch.nkn@gmail.com>
2019-12-12 22:20:10 -0500

    Draft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = 'latest'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,7 +49,6 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
